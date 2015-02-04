ac46269f6a8988704fb4db67920c74f6910056a3
xhaller <bingxin030729@163.com>
2015-02-04 22:32:32 +0800

    version2.0

--- pedometer.ino
+++ pedometer.ino
@@ -3,92 +3,66 @@
 #include <FIMU_ITG3200.h>
 #include <Wire.h>
 
-#define FILTER_N 12 
+#define FILTER_N 12
+
 FreeSixIMU sixDOF = FreeSixIMU();
 int rawSixDof[6];
-float oldxx,oldyy,oldzz,olddd;
-int StepNumber0=0,StepNumber1=0;
-void setup() 
-{ 
-  Serial.begin(9600);
-  Wire.begin();
-   
-  delay(5);
-  sixDOF.init();   
-  delay(5);
+float old_accx, old_accy, old_accz, old_dot;
+int StepNum = 0, StepNumber = 0;
+
+void setup() {
+Serial.begin(9600);
+Wire.begin();
+
+delay(5);
+sixDOF.init();
+delay(5);
 }
+
 void loop() {
-  float xx = Filter_x();
-  float yy = Filter_y();
-  float zz = Filter_z();
-  float dot = (oldxx * xx)+(oldyy * yy)+(oldzz * zz);
-  float olda = abs(sqrt(oldxx*oldxx+oldyy*oldyy+oldzz*oldzz));
-  float newa = abs(sqrt(xx*xx+yy*yy+zz*zz));
-  dot /=(olda*newa);
-  StepNumber0=StepNumber1;
-  if(abs(dot-olddd) >= 0.05)
-  {bool isChange;
-    if(!isChange){isChange = 1;
-  StepNumber1 +=1;}
-  else isChange = 0;
-  }
-  oldxx = xx;
-  oldyy = yy;
-  oldzz = zz;
-  olddd = dot;
-  delay(60);
-  
-  if(StepNumber0!=StepNumber1)
-  {
-  Serial.println(StepNumber1);
-  }
-  
-  delay(100); 
-        
- 
+float accx = Filter(0);//读取滤波后的加速度x分量
+float accy = Filter(1);
+float accz = Filter(2);
+float dot = (old_accx * accx)+(old_accy * accy)+(old_accz * accz);
+float old_acc = abs(sqrt(old_accx*old_accx+old_accy*old_accy+old_accz*old_accz));
+float new_acc = abs(sqrt(accx*accx+accy*accy+accz*accz));
+dot /= (old_acc * new_acc);//计算加速度变化程度
+
+StepNum = StepNumber;
+if(abs(dot - old_dot) >= 0.05) {
+//变化程度超过阈值，则判定步数增加
+bool isChange;
+if(!isChange) {
+isChange = 1;
+StepNumber += 1;
+}
+else isChange = 0;
+}
+old_accx = accx;
+old_accy = accy;
+old_accz = accz;
+old_dot = dot;
+delay(60);
+
+if(StepNum != StepNumber) {
+//步数增加则打印出来
+Serial.println(StepNumber);
+}
+
+delay(100);
 }
 
 int filter_buf[FILTER_N + 1];
-float Filter_x() {
-  int i;
-  float filter_sum = 0.0;
-  
-  for(i = 0; i < FILTER_N; i++) {
-    sixDOF.getRawValues(rawSixDof);
-    filter_buf[i] = rawSixDof[0];
-    
-    filter_sum += filter_buf[i];
-    delay(5);
-  }
-  return (float)(filter_sum / FILTER_N);
+
+float Filter(int s) {
+//平均滤波法
+float filter_sum = 0.0;
+
+for(int i = 0; i < FILTER_N; i++) {
+sixDOF.getRawValues(rawSixDof);
+filter_buf[i] = rawSixDof[s];
+filter_sum += filter_buf[i];
+delay(5);
 }
- 
-float Filter_y() {
-  int i;
-  float filter_sum = 0.0;
-  
-  for(i = 0; i < FILTER_N; i++) {
-    sixDOF.getRawValues(rawSixDof);
-    filter_buf[i] = rawSixDof[1];
-    
-    filter_sum += filter_buf[i];
-    delay(5);
-  }
-  return (float)(filter_sum / FILTER_N);
+return (float)(filter_sum / FILTER_N);
 }
- 
-float Filter_z() {
-  int i;
-  float filter_sum = 0.0;
-  
-  for(i = 0; i < FILTER_N; i++) {
-    sixDOF.getRawValues(rawSixDof);
-    filter_buf[i] = rawSixDof[2];
-    
-    filter_sum += filter_buf[i];
-    delay(5);
-  }
-  return (float)(filter_sum / FILTER_N);
-}
- 
- 
